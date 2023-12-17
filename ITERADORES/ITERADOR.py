f40442122c30b56b9fe286ba1caf91941a6cbbae
CesarSoria2224 <130561780+CesarSoria2224@users.noreply.github.com>
2023-12-16 21:49:09 -0400

    ciclos for

---
+++ ITERADORES/ITERADOR.py
@@ -0,0 +1,8 @@
+lenguajes = ('PHYTHON', 'C++', 'C#','JAVA')
+for i in lenguajes:
+    print(f"los lenguajes son: {i}")
+
+a = 10
+# este es un for que enumera 
+for i in range(0, a+1 ,2):
+    print (f"numeros:{i}")
